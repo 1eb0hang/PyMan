947fcd898ace780ab69dec7450f23de1b36f04e0
Lebohang <lebohangmokhwibitjane@gmail.com>
2023-07-20 15:55:16 +0200

    cleaned up a bit, Need to implement letter vs word selection

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 word = "hippopotamus"
 attempts = 7
 attemptNo = 1
-correct = False
-end = False
+correct = False #check if guess is correct
+valid = False #check if input is valid; will be reset when nessesary
+end = False  #check if attempts have been used up
 
-def LetterCount(strWord: str) -> int:
+def strlen(strWord: str) -> int:
     result = 0
     for i in strWord:
         result +=1
     
     return result
 
-count = LetterCount(word)
+count = strlen(word)
 print("\nThe word has " + str(count) +" letters\n")
 
+valid = False
+while(valid == False):
+
+    guessType = input("Would you like to guess a letter or a word:\n[1] Letter\n[2] Word\n>> ")
+    
+    if(guessType == "1" or guessType == "2"):
+        gType = int(guessType)
+        print(type(type))
+        valid = True
+    else:
+        print("You have entered an invalid input\n")
+
 while(end == False and correct == False):
-    guess = input("Please guess the word:\n -> ")
+    guess = input("Please guess the word:\n>> ")
     
     if(guess == word):
         print("Correct!\n")
--- other.py
+++ other.py
@@ -1,5 +1,14 @@
+#displays text as "crossed out"
 def strike(text): 
     result = ''
     for c in text:
         result = result + c + '\u0336'
+    return result
+
+#displays number of characters in a string
+def strlen(strWord: str) -> int:
+    result = 0
+    for i in strWord:
+        result +=1
+    
     return result
