d97e553e0b65086eb8de691b6df06e7efaeb05e9
Lebohang <lebohangmokhwibitjane@gmail.com>
2023-07-20 15:16:06 +0200

    Now have 7 trys to get it

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 word = "hippopotamus"
 attempts = 7
+attemptNo = 1
+correct = False
+end = False
 
-guess = input("Please guess the word:\n -> ")
-
-if(guess == word):
-    print("Correct!\n")
-else:
-    print("Unfortunatly that is incorrect :(\n")
-
+while(end == False and correct == False):
+    guess = input("Please guess the word:\n -> ")
+    
+    if(guess == word):
+        print("Correct!\n")
+        correct = True
+        end = True
+    elif(guess == "\n"):
+        print("Unfortunatly that is incorrect :(\n")
+       
+    else:
+        print("Unfortunatly that is incorrect :(\n")
+    
+    attemptNo += 1
+    
+    if(attemptNo>attempts):
+        end = True
+        
+    
 print("End of program!\n")
