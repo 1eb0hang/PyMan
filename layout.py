d49b24ccd9d01688b506d7f72431d0932299b8fc
Lebohang <lebohangmokhwibitjane@gmail.com>
2023-07-22 16:00:39 +0200

    had to leave; think of as a half commit

--- layout.py
+++ layout.py
@@ -9,3 +9,7 @@ def clear():
     # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
+
+def PlayScreen(guessType :Guess, chance: int): #TODO : create some centralized place for such functionality
+    
+    
