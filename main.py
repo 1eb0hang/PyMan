40291ffa4d0b2ac5b819df637af682fcc5c6fd6a
Lebohang <lebohangmokhwibitjane@gmail.com>
2023-07-22 13:36:31 +0200

    added GUESS enum class for guess type branching

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from enum import Enum
+
 word = "hippopotamus"
 attempts = 7
 attemptNo = 1
 correct = False #check if guess is correct
 valid = False #check if input is valid; will be reset when nessesary
 end = False  #check if attempts have been used up
+
+class Guess(Enum):
+    LETTER = 1
+    WORD = 2
+
 
 def strlen(strWord: str) -> int:
     result = 0
@@ -21,27 +28,29 @@ while(valid == False):
     guessType = input("Would you like to guess a letter or a word:\n[1] Letter\n[2] Word\n>> ")
     
     if(guessType == "1" or guessType == "2"):
-        gType = int(guessType)
+        gType = Guess(int(guessType))
         print(type(type))
         valid = True
     else:
         print("You have entered an invalid input\n")
 
-while(end == False and correct == False):
-    guess = input("Please guess the word:\n>> ")
-    
-    if(guess == word):
-        print("Correct!\n")
-        correct = True
-        end = True
-       
-    else:
-        print("Unfortunatly that is incorrect :(\n")
-    
-    attemptNo += 1
-    
-    if(attemptNo>attempts):
-        end = True
+print(gType)
+
+#while(end == False and correct == False):
+#    guess = input("Please guess the word:\n>> ")
+#    
+#    if(guess == word):
+#        print("Correct!\n")
+#        correct = True
+#        end = True
+#       
+#    else:
+#        print("Unfortunatly that is incorrect :(\n")
+#    
+#    attemptNo += 1
+#    
+#    if(attemptNo>attempts):
+#        end = True
         
     
 print("End of program!\n")
